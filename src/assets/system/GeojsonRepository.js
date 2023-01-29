a9c11ad8dd44c4a2267cd0846474d7db9998ba00
VicentGN <vicentgarcian@gmail.com>
2023-01-29 15:47:43 +0100

    REMOVE: data from heroku

--- src/assets/system/GeojsonRepository.js
+++ src/assets/system/GeojsonRepository.js
@@ -1,19 +1,23 @@
 var geojsonRepository = [
 	{
+		/** 
 		id: "1",
 		id_2: 'hospitales',
 		title: "Hospitales de España",
 		description: "Muestra los hospitales público-privados de España",
 		url: "https://cartovis-server.herokuapp.com/hospitales",
-		attribution: "ESRI ESPAÑA",
+		attribution: "ESRI ESPAÑA"
+		**/
   },
   {
+	  /** 
 		id: "2",
 		id_2: 'guardia_civil',
 		title: "Cuarteles de la Guardia Civil",
 		description: "Localización de las comandancias y puestos auxiliares de la Guardia Civil",
 		url: "https://cartovis-server.herokuapp.com/guardiacivil",
-		attribution: "ESRI ESPAÑA",
+		attribution: "ESRI ESPAÑA"
+		**/
   }
 ];
 
